f1c2ecafef0e1c31870e6989c8f180e5ed21d198
Chirag Rana <125183994+Chiragrana17@users.noreply.github.com>
2024-11-17 19:33:48 +0530

    Update api.js

--- CarHub/frontend/src/api.js
+++ CarHub/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5006/api";
+const API_URL = "https://carhub-3-3c5y.onrender.com";
 
 // Authentication
 export const login = async(email, password) => {
